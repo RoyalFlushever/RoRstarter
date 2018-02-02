9dbcc5797f0eb44a03e08764d8fa18d7ec198d62
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-02-02 09:22:52 -0500

    complete advanced setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	root 'static_pages#home'
+	
 	get 'static_pages/home'
 
 	get 'static_pages/help'
@@ -10,6 +13,4 @@ Rails.application.routes.draw do
 	resources :users
 	get 'welcome/index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
 end
