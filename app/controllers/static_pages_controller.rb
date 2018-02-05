3efe8ea56326d9c530e5cbf7fac604e8720472c5
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-02-05 13:03:17 -0500

    url path signup form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -17,4 +17,8 @@ class StaticPagesController < ApplicationController
   def login
     
   end
+
+  def signup
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
   resources :microposts
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	root 'static_pages#login'
+	root 'static_pages#home'
 	
-	get 'static_pages/home'
+	get '/home', to: 'static_pages#home'
 
-	get 'static_pages/help'
+	get '/login', to: 'static_pages#login'
 
-	get 'static_pages/about'
+	get '/help', to: 'static_pages#help'
 
-	get 'static_pages/contact'
+	get '/about', to: 'static_pages#about'
+
+	get '/contact', to: 'static_pages#contact'
+
+	get '/signup', to: 'static_pages#signup'
 
 	resources :users
 	get 'welcome/index'
