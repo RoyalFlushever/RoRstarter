2ccb78e3ae188c39d2613f490a18db5e167d448d
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-02-05 13:29:25 -0500

    generate model, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   resources :microposts
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root 'static_pages#home'
@@ -15,7 +17,6 @@ Rails.application.routes.draw do
 
 	get '/signup', to: 'static_pages#signup'
 
-	resources :users
 	get 'welcome/index'
 
 end
