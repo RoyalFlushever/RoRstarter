2608f04d6ef34ab0a0e913909b92caac9fcf89dc
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-02-05 14:13:59 -0500

    adding validation and indices

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'users/new'
 
-  resources :microposts
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root 'static_pages#home'
 	
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180202151541) do
+ActiveRecord::Schema.define(version: 20180205190849) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "context"
