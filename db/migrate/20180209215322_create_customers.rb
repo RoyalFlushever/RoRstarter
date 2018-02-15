36b1522caccd799c40ace0e236e1e8f54c5d371f
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-02-15 03:47:23 -0500

    publish to github

---
+++ db/migrate/20180209215322_create_customers.rb
@@ -0,0 +1,13 @@
+class CreateCustomers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :customers do |t|
+      t.string :provider
+      t.string :uid
+      t.string :name
+      t.string :oauth_token
+      t.datetime :oauth_expires_at
+
+      t.timestamps
+    end
+  end
+end
