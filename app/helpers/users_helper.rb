b1b1621f469e448ffc4413ff58cb77cbba5a1876
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-03-14 20:13:50 -0400

    show user page mock up

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,12 @@ module UsersHelper
 	def gravatar_for(user)
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase) 	
 		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-		image_tag(gravatar_url, alt: user.name, class: "gravatar")
+		image_tag(gravatar_url, alt: user.name, class: "gravatar", size: "200")
+	end
+
+	# Returns the date ago posted
+	def posted_ago(user)
+		post_time = ((Time.now - @user.created_at) / (60 * 24 * 24)).round()
 	end
 
 end
