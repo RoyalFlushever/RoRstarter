022888b48eaede11ef57fde1514f2a6dd16c4434
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-03-22 06:58:22 -0400

    user remember_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,13 +13,29 @@ class User < ApplicationRecord
   has_secure_password      
   validates :password, length: { minimum: 6 }, presence: true
 
-  # Returns a Random token
-  def User.new_token
-  	SecureRandom.urlsafe_base64
-  end
+  class << self
+	  # Returns the hash digest of the given string
+	  def self.digest
+	 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+	 																								  BCrypt::Engine::cost	
+	 		BCrypt::Password.create(string, cost: cost)
+	  end
 
+
+	  # Returns a Random token
+	  def self.new_token
+	  	SecureRandom.urlsafe_base64
+	  end
+
+  # Remembers a user in the database for use in persistent sessions
   def remember
-  	
+  	self.remember_token = User.new_token
+  	update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+  # Returns true if the given token matches the digest.
+  def authenticate?
+  	BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
 end
