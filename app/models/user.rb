031974f131acd50685e8a3d22c30f7cdee7a596c
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-03-22 06:00:37 -0400

    add remember_token to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
 
+  attr_accessor :remember_token
   before_save { self.email = email.downcase } #some database adapters use case-sensitive indices
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -11,5 +12,14 @@ class User < ApplicationRecord
 
   has_secure_password      
   validates :password, length: { minimum: 6 }, presence: true
+
+  # Returns a Random token
+  def User.new_token
+  	SecureRandom.urlsafe_base64
+  end
+
+  def remember
+  	
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180209215322) do
+ActiveRecord::Schema.define(version: 20180322095349) do
 
   create_table "customers", force: :cascade do |t|
     t.string "provider"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20180209215322) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
